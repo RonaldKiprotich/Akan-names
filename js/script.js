7943b8d7b3ec1c1c510ffd8667a21634ed4db15c
RonaldKiprotich <protich12@gmail.com>
2020-08-23 19:01:33 +0300

    added js and modified formbox background color

--- js/script.js
+++ js/script.js
@@ -17,3 +17,9 @@ function findAkanNames(){
     } else if(year <= 0){
         alert("Please enter a valid year")
     }
+
+
+    var a = Math.floor((14 - month) / 12)
+    var y = year - a
+    var m = month + 12 * a - 2
+    var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m )/ 12)) % 7;
